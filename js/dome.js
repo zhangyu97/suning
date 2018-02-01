d1695d561f201be89cc2826ee4242f3c5d139782
zhangyu97 <396471308@qq.com>
2018-02-01 23:05:00 +0800

    js

--- js/dome.js
+++ js/dome.js
@@ -112,6 +112,77 @@
     // console.log(l_jian);
     // let r_jian = document.querySelector(".juhui_youjian");
     // console.log(r_jian);
+        // 楼层跳转
+        // 返回顶部
+        let back = document.querySelector(".tten");
+        back.onclick =function () {
+            animate(document.body,{scrollTop:0},500);
+            animate(document.documentElement,{scrollTop:0},500);
+        }
+        let lian =document.querySelector(".lianjie");
+        lian.style.display="none";
+        console.log(lian);
+        let flag1 = true;
+        let flag2 =true;
+        // 获取页面的高度
+        let height = document.documentElement.clientHeight;
+        window.onscroll = function (){
+            // 获取要跳转的每个楼层
+            let floor = document.querySelectorAll(".floor");
+            // 获取楼层按钮
+            let lianjie = document.querySelectorAll(".lianjie ul li.he");
+            // 获取hidden出现的高度   banner下
+            let banhe = document.querySelector(".juhui");
+            // 获取hidden
+            let hidden = document.querySelector(".hidden");
+            // 获取链接出现是位置
+            let su = document.querySelector(".suning");
+            let tops = document.body.scrollTop? document.body.scrollTop:document.documentElement.scrollTop;
+            // hidden出现的位置
+            if(tops > banhe.offsetTop){
+                if(flag1){
+                    flag1 = false;
+                    animate(hidden,{top:0},500,function () {
+                        flag2 = false;
+                    })
+                }
+            }else{
+                if(!flag2){
+                    flag2 = false;
+                    animate(hidden,{top:-50},500,function () {
+                        flag1 = true;
+                    })
+                }
+            }
+            // 链接出现的位置
+            if(tops >su.offsetTop+260){
+                lian.style.display="block";
+            }else if(tops <su.offsetTop){
+                lian.style.display="none";
+            }
+            floor.forEach(function (fv,fi) {
+                // 滚动条的高度>=楼层距离窗口高度;
+                if(tops>=fv.offsetTop-height+120){
+                    lianjie.forEach(function (lv,li) {
+                        lv.classList.remove("active");
+                    })
+                    lianjie[fi].classList.add("active");
+                }else if(tops<height-120){
+                    lianjie.forEach(function (ll,li) {
+                        ll.classList.remove("active");
+                    })
+                }
+            })
+            // 要出现的楼层跳转，遍历lianjie
+            lianjie.forEach(function (liv,lii) {
+                liv.onclick = function () {
+                    // 保存一下值连接
+                    let bao = floor[lii].offsetTop;
+                    animate(document.body,{scrollTop:bao-70},100);
+                    animate(document.documentElement,{scrollTop:bao-70},100);
+                }
+            })
+        }
 
 
 
